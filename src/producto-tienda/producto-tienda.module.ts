150d1fec3cf33ad0122ceff5a363e24345bf5695
Mateo <ivan.castano@globant.com>
2022-09-18 00:05:12 -0500

    [ADD]Controller to association

--- src/producto-tienda/producto-tienda.module.ts
+++ src/producto-tienda/producto-tienda.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductoEntity } from 'src/producto/producto.entity';
 import { TiendaEntity } from 'src/tienda/tienda.entity';
 import { ProductoTiendaService } from './producto-tienda.service';
+import { ProductoTiendaController } from './producto-tienda.controller';
 
 @Module({
   providers: [ProductoTiendaService],
   imports: [TypeOrmModule.forFeature([TiendaEntity, ProductoEntity])],
+  controllers: [ProductoTiendaController],
 })
 export class ProductoTiendaModule {}
